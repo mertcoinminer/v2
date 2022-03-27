7402169d9039f3110f934aee871cb7397452dad2
mertcoinminer <101499910+mertcoinminer@users.noreply.github.com>
2022-03-27 21:25:23 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+function stop() 
+{window.open("about:blank","_self").close();}
+
+function getVideoCardInfo() {
+     const gl = document.createElement('canvas').getContext('webgl');
+     if (!gl) {
+       console.error("WebGL is not supported.");
+       throw ("WebGL is not supported.");
+       return {
+         error: "no webgl",
+       };
+     }
+     const debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
+     return debugInfo ? {
+       vendor: gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL),
+       renderer:  gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL),
+     } : {
+       error: "no WEBGL_debug_renderer_info",
+     };
+   }
+   
